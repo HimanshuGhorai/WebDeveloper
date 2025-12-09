e510d905340ed8b01c2b6acea50a2392cc3e62db
himanshu Ghorai <himanshughorai000@gmail.com>
2025-12-09 19:36:06 +0530

    datatype

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// var let and const }for the variable decleration key word;
+
+// var variablekey = "this is var";
+
+// console.log(variablekey);
+
+// decleration and initialization
+
+var a; //initialization
+var name = "himu"; // initialization and declaraton both;
+
+// hosting
+
+console.log(a);
+var a = 12;
+
+// console.log(b);
+// let b = 23;
+
+// console.log(c);
+// const c = 33;
+
+// typeof all datatype :----
+
+// prmitive datatype and non prmitive data type ;
+
+// prmitive data type is : string,number,boolean,null,symbol,bigint;'
+// string
+let stringName = "himanshu";
+console.log(stringName);
+
+// not prmitive data type is: array,objects,function,
